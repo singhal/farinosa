8e62f29101f8836527dd8317637012500af54f8a
singhal <sonal.singhal1@gmail.com>
2024-05-09 06:18:08 -0700

    modification to add panel B

--- scripts/figures/Figure3.R
+++ scripts/figures/Figure3.R
@@ -90,9 +90,11 @@ c = c %>% mutate(cluster1 = cutree(fit, h = 1),
 length(unique(c$cluster1))
 length(unique(c$cluster2))
 
-c5 = c %>% group_by(cluster2) %>% summarize(yellow=sum(yellow, na.rm=T), 
-                                                                   black = sum(black, na.rm=T), 
-                                                                   latitude = mean(latitude), longitude = mean(longitude)) %>% 
+c5 = c %>% group_by(cluster2) %>% 
+  summarize(yellow=sum(yellow, na.rm=T), 
+            black = sum(black, na.rm=T), 
+            latitude = mean(latitude),
+            longitude = mean(longitude)) %>% 
   ungroup() %>% mutate(total = yellow + black) %>%
   mutate(percent_black = black / total)
 
@@ -103,6 +105,7 @@ c5$distance = geosphere::distHaversine(c5[, c("longitude", "latitude")], start) 
 
 cl1 = hzar.doMolecularData1DPops(c5$distance, c5$percent_black, c5$total)
 cl1res = make_cline(cl1)
+cl1res[[1]]$ML.cline$param.free
 range(unlist(lapply(hzar.getCredParamRed(cl1res[[1]])$clines, 
                     function(x) x$param.all$width)))
 
@@ -137,6 +140,7 @@ a3$distance = geosphere::distHaversine(a3[, c("longitude", "latitude")], start) 
 
 cl2 = hzar.doMolecularData1DPops(a3$distance, a3$population2, a3$n)
 cl2res = make_cline(cl2)
+cl2res[[1]]$ML.cline$param.free
 range(unlist(lapply(hzar.getCredParamRed(cl2res[[1]])$clines, 
                     function(x) x$param.all$width)))
 
@@ -151,14 +155,9 @@ pts = c[, c("longitude", "latitude")]
 clim = raster::extract(climr, pts)
 
 clim2 = clim[complete.cases(clim), ]
-pts2 = pts[complete.cases(clim),]
+pts2 = c[complete.cases(clim),] %>% dplyr::select(latitude, longitude, yellow, black)
 
-cc = cbind(clim2, pts2)
 xpca = pca(clim2, scale="uv", center = T, nPcs = 10)
-
-summary(xpca)
-sort(abs(xpca@loadings[, "PC1"]))
-rev(sort(abs(xpca@loadings[, "PC2"])))
 
 #distance matrix
 dist.in.km.matrix3 <- fields::rdist.earth(pts2, miles = F, R=6371)
@@ -167,34 +166,71 @@ fit3 <- hclust(as.dist(dist.in.km.matrix3), method = "single")
 # group by 1 vs 5 km
 pts3 = pts2 %>% mutate(cluster1 = cutree(fit3, h = 1), 
                        cluster2 = cutree(fit3, h = 5))
-pts3 = cbind(pts3, xpca@scores[ , 1:2])
+pts3 = cbind(pts3, clim2, xpca@scores[ , 1:2])
 
-# explore raw data
-colnames(clim) = gsub("wc2.0_bio_30s_", "", gsub("_Encelia", "", names(climr)))
-r = c %>% mutate(per_black = black / (yellow+black)) %>%
-  dplyr::select(per_black, cluster1) %>%
-  cbind(clim) %>% 
-  group_by(cluster1) %>% summarise_all(mean)
+
+pts4 = pts3 %>% group_by(cluster2) %>% 
+  mutate(yellow = sum(yellow, na.rm=T), 
+            black = sum(black, na.rm=T), 
+            latitude = mean(latitude),
+            longitude = mean(longitude)) %>% 
+  ungroup() %>% group_by(cluster2) %>% 
+  mutate(per_black = black / (yellow+black), n = n()) %>%
+  summarize_all(mean) %>% 
+  ungroup()
+pts4$distance = geosphere::distHaversine(pts4[, c("longitude", "latitude")], start) / 1000
+names(pts4) = gsub("wc2.0_bio_30s_", "bioclim",
+                   gsub("_Encelia", "", names(pts4)))
+vars = c(paste0("bioclim", formatC(seq(1, 19), width = 2, format = "d", flag = "0")), "PC1", "PC2")
+for (var in c(vars)) {
+  var2 = paste0(var, "_normal")
+  pts4[,var2] = (pts4[,var]-min(pts4[,var]))/(max(pts4[,var])-min(pts4[,var]))
+}
+vars2 = paste0(vars, "_normal")
+
+res = vector("list", length(vars2))
+names(res) = vars2
+for (i in 12:length(vars2)) {
+  var = vars2[i]
+  slope = lm(pts4$per_black ~ pull(pts4[,var]))$coefficients[2]
+  if (slope > 1) {
+    vals = pull(pts4[,var])
+  } else {
+    vals = 1 - pull(pts4[,var])
+  }
+  cl3 = hzar.doMolecularData1DPops(vals, pts4$per_black, pts4$n)
+  fit = make_cline(cl3)
+  predval = fit[[1]]$ML.cline$clineFunc(vals)
+  obsval = pts4$per_black
+  error = sum((predval - obsval) ^ 2)
+  res[[var]] = list(fit, error)
+  plot(vals, obsval, main = var)
+  points(vals, predval, pch = 16, col ="red")
+}
 
 bc = read.csv("bioclim.csv")
+tt = data.frame(sort(unlist(lapply(res, function(x) x[[2]]))))
+names(tt) = "sum squared residuals"
+rownames(tt) = gsub("_normal", "", rownames(tt))
+tt$description = bc[match(parse_number(rownames(tt)), bc$var), "meaning"]
+tt["PC1", "description"] = "PC1"
+tt["PC2", "description"] = "PC2"
+write.csv(tt, "figures/Table_S2.csv")
 
-order2 =  gsub("wc2.0_bio_30s_", "",
-              gsub("_Encelia", "",
-              names(rev(sort(abs(xpca@loadings[, "PC2"]))))))
-order3 = bc[match(as.numeric(order2), bc$var), "meaning"]
+load = data.frame(xpca@loadings[,1:2])
+rownames(load) = gsub("wc2.0_bio_30s_", "bioclim",
+                   gsub("_Encelia", "", rownames(load)))
+load$description = bc[match(parse_number(rownames(load)), bc$var), "meaning"]
+load$PC1 = round(load$PC1, 3)
+load$PC2 = round(load$PC2, 3)
+write.csv(load, "figures/Table_S3.csv")
 
-res = vector("list", length = 18)
-r2 = r %>% gather(bioclim, value, -cluster1, -per_black)
-r2$bioclim = bc[match(as.numeric(r2$bioclim), bc$var), "meaning"]
-r2$bioclim2 = factor(r2$bioclim, levels = order3)
+# explore raw data
+r2 = pts4 %>% dplyr::select(vars, cluster2, per_black) %>% 
+  gather(bioclim, value, -cluster2, -per_black)
+r2$bioclim = tt[match(r2$bioclim, rownames(tt)), "description"]
+r2$bioclim2 = factor(r2$bioclim, levels = tt$description)
 
-# for (i in 1:length(order2)) {
-#   var = order2[i]
-#   res[[i]] = ggplot(r, aes(.data[[var]], per_black)) + 
-#     geom_point() + theme_classic() +
-#     ylab("% black")
-# }
-# plts = cowplot::plot_grid(plotlist = res, ncol = 5)
 rr = ggplot(r2, aes(value, per_black)) +
   geom_point() + 
   ylab("% brown") + 
@@ -204,41 +240,11 @@ rr = ggplot(r2, aes(value, per_black)) +
         strip.background = element_blank())
 cowplot::save_plot("figures/FigureSXX_bioclim.png", rr, base_width = 8, base_height = 10)
 
-library(corrplot)
-M = cor(r, use = "pairwise.complete.obs")
-corrplot.mixed(M, order = 'AOE')
-
-pts4 = pts3 %>% group_by(cluster2) %>% 
-  summarize(PC1 = mean(PC1), PC2 = mean(PC2),  
-            n = n(), latitude = mean(latitude), 
-            longitude = mean(longitude)) %>% 
-  ungroup()
-
-pts4$distance = geosphere::distHaversine(pts4[, c("longitude", "latitude")], start) / 1000
-pts4$PC2_normal = (pts4$PC2-min(pts4$PC2))/(max(pts4$PC2)-min(pts4$PC2))
-pts4$PC1_normal = (pts4$PC1-min(pts4$PC1))/(max(pts4$PC1)-min(pts4$PC1))
-
-supfig = ggplot() + geom_point(data = pts4, aes(distance, PC1_normal), col = "red") +
-  geom_line(data = cl1res[[2]], aes(distance, percent_black), col = "#482624") +
-  theme_classic() + xlab("distance (km)") + 
-  # Custom the Y scales:
-   scale_y_continuous(
-    # Add a second axis and specify its features
-    sec.axis = sec_axis( trans=~.*1, name="% brown")
-  ) +
-  ylab("normalized PC1") +
-  theme(
-  axis.title.y = element_text(color = "black", size=13),
-  axis.title.y.right = element_text(color = "red", size=13)
-  ) 
-cowplot::save_plot("figures/SupplFigX_PC1climate.png", supfig)
-  
-
 cl3 = hzar.doMolecularData1DPops(pts4$distance, pts4$PC2_normal, pts4$n)
 cl3res = make_cline(cl3)
+cl3res[[1]]$ML.cline$param.free
 range(unlist(lapply(hzar.getCredParamRed(cl3res[[1]])$clines, 
                     function(x) x$param.all$width)))
-
 
 cl1res[[2]]$type = "% brown flower"
 cl2res[[2]]$type = "% gen. pop. 2"
@@ -253,15 +259,20 @@ px = pts4 %>% mutate(type = "PC2 climate") %>%
   dplyr::select(distance = distance, type = type, value = PC2_normal)
 ptsx = rbind(cx, ax, px)
 
-
-plt = ggplot() +
+a = ggplot() +
   geom_point(data = ptsx, aes(distance, value, color = type), alpha = 0.5, shape = 16) +
   geom_line(data = predval, aes(distance, percent_black, color = type)) +
   scale_color_manual(values = RColorBrewer::brewer.pal(3, "Set2")) +
   xlab("distance (km)") +
-  theme_classic() +
-  theme(legend.title = element_blank(), legend.position = c(0.8, 0.2))
-cowplot::save_plot("figures/Figure3.png", plt, base_height = 3, base_width = 4)
+  theme_classic() + 
+  theme(legend.title = element_blank(), legend.position = c(0.75, 0.2))
+b = ggplot(pts4) + 
+  geom_point(aes(bioclim17, per_black)) +
+  xlab("precip. in driest quarter") +
+  ylab("% brown") +
+  theme_classic()
+ab = plot_grid(a, b, rel_widths = c(0.6, 0.4), labels = c("A", "B"))
+cowplot::save_plot("figures/Figure3.png", ab, base_height = 3, base_width = 6.5)
 
 
 #################
